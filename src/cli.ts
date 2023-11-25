59c4003241317c7f6062dccf6e88130d5398f1d2
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-26 02:03:30 +0800

    feat: add delay function to CLI

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,13 @@
-console.log('Hello World from CLI');
+async function delay(timer:number) {
+	return new Promise<number>((resolve) => {
+		setTimeout(() => {
+			resolve(timer);
+		}, timer);
+	});
+}
+
+delay(500).then(
+	() => {
+		console.log('Hello World from CLI');
+	}
+);
